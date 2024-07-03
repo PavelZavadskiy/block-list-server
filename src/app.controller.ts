769c4cdc9092096355c296e85e8612c65ceaed7c
Pavel <87939143+PavelZavadskiy@users.noreply.github.com>
2024-07-03 12:19:31 +0300

    Added prisma init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
+import { DbService } from './db/db.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
   async getHello(): Promise<string> {
-    const users = await prisma.user.findMany({});
+    const users = await this.dbService.user.findMany({});
     console.log(users);
     return this.appService.getHello();
   }
